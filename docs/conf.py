4687c3377ad586f7e911aa66e805994d3dc2fcd0
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2021-07-15 17:59:26 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,12 @@ html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#3DB4FF",
         "color-brand-content": "#3DB4FF",
+        "font-stack--monospace": "Iosevka, monospace",
     },
     "dark_css_variables": {
         "color-brand-primary": "#3DB4FF",
         "color-brand-content": "#3DB4FF",
+        "font-stack--monospace": "Iosevka, monospace",
     },
 }
 
