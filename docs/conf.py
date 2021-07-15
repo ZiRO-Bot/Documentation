44b7b9dcee7bd74ff04c4277983c46d4c1bc43a9
ziro <palembani@gmail.com>
2021-07-15 18:59:30 +0700

    + trying to add font pt3

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,12 @@ html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#3DB4FF",
         "color-brand-content": "#3DB4FF",
-        "font-stack--monospace": "Iosevka, monospace",
+        "font-stack--monospace": "Iosevka Web, monospace",
     },
     "dark_css_variables": {
         "color-brand-primary": "#3DB4FF",
         "color-brand-content": "#3DB4FF",
-        "font-stack--monospace": "Iosevka, monospace",
+        "font-stack--monospace": "Iosevka Web, monospace",
     },
 }
 
